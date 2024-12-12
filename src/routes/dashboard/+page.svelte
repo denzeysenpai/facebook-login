1dd08ae6177cc06a91c154230959b191ae93b861
DenzeySenpai <marksolante221@gmail.com>
2024-12-12 13:45:00 +0800

    [UPDATE 0.0.1 : INITIALIZATION]

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import "./style.css";
+</script>
+
+<svelte:head>
+    <title>Welcome to Dashboard!</title>
+</svelte:head>
+
+<h1>Welcome!</h1>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import "./style.css";
+</script>
+
+<svelte:head>
+    <title>Login using facebook</title>
+</svelte:head>
+
+<form action="">
+    <label for="username">Username</label>
+    <input type="text" id="username" name="username">
+    <label for="password">Password</label>
+    <input type="text" id="password" name="password">
+    <button type="submit">Login</button>
+</form>
